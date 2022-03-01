e9c10494a078c9cab543f6a12ee9d3d72278ee04
Olivia Chang <o.chang@berkeley.edu>
2022-02-28 16:57:41 -0800

    removing lab6 temp

--- materials/sp22/lab/lab06/tests/q1_2.py
+++
@@ -1,8 +0,0 @@
-test = {   'name': 'q1_2',
-    'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> 0 <= expected_proportion_correct <= 1\nTrue', 'hidden': False, 'locked': False},
-                                   {'code': '>>> expected_proportion_correct == 0.5\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
--- materials/sp22/lab/lab06/tests/q1_3.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q1_3',
-    'points': None,
-    'suites': [   {   'cases': [{'code': '>>> type(valid_stat) == np.ndarray\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> int(sum(valid_stat))\n2', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
