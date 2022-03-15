16855a2ed36d8fc46fa945b8fe983e791460ced7
Raymond Wang <raymondwang@berkeley.edu>
2022-03-14 20:31:29 -0700

    releasing lab07

--- materials/sp22/lab/lab07/tests/q2_10.py
+++ materials/sp22/lab/lab07/tests/q2_10.py
@@ -1,1 +1,3 @@
-test = {'name': 'q2_10', 'points': None, 'suites': [{'cases': [{'code': '>>> 0 <= empirical_p < 0.05\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q2_10',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> 0 <= empirical_p < 0.05\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- materials/sp22/lab/lab07/tests/q2_8.py
+++ materials/sp22/lab/lab07/tests/q2_8.py
@@ -1,9 +1,12 @@
 test = {   'name': 'q2_8',
     'points': None,
-    'suites': [   {   'cases': [   {'code': '>>> test_stat = round(simulate_and_test_statistic(bakers, "won", "star baker awards"), 3)\n>>> -2 < test_stat < 2\n', 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {   'code': '>>> test_stat = round(simulate_and_test_statistic(bakers, "won", "star baker awards"), 3)\n>>> -2 < test_stat < 2\nTrue',
+                                       'hidden': False,
+                                       'locked': False},
                                    {   'code': '>>> np.random.seed(1)\n'
                                                '>>> test_stat2 = simulate_and_test_statistic(bakers, "won", "star baker awards")\n'
-                                               '>>> np.round(test_stat2, 3) == -0.023 or np.round(test_stat2, 3) == -0.132\n',
+                                               '>>> np.round(test_stat2, 3) == -0.023 or np.round(test_stat2, 3) == -0.132\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
